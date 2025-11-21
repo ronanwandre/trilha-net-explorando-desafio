0a47e8bbef9d4004f0b6967352f5ba81cc52efac
RonanWandre <ronanwsdf@gmail.com>
2025-11-20 23:15:01 -0300

    Resolvendo o desafio

--- Program.cs
+++ Program.cs
@@ -3,7 +3,6 @@ using DesafioProjetoHospedagem.Models;
 
 Console.OutputEncoding = Encoding.UTF8;
 
-// Cria os modelos de hóspedes e cadastra na lista de hóspedes
 List<Pessoa> hospedes = new List<Pessoa>();
 
 Pessoa p1 = new Pessoa(nome: "Hóspede 1");
@@ -12,14 +11,36 @@ Pessoa p2 = new Pessoa(nome: "Hóspede 2");
 hospedes.Add(p1);
 hospedes.Add(p2);
 
-// Cria a suíte
 Suite suite = new Suite(tipoSuite: "Premium", capacidade: 2, valorDiaria: 30);
 
-// Cria uma nova reserva, passando a suíte e os hóspedes
 Reserva reserva = new Reserva(diasReservados: 5);
 reserva.CadastrarSuite(suite);
 reserva.CadastrarHospedes(hospedes);
 
-// Exibe a quantidade de hóspedes e o valor da diária
 Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes()}");
 Console.WriteLine($"Valor diária: {reserva.CalcularValorDiaria()}");
+
+Console.WriteLine("\n--- Teste com desconto (10 dias ou mais) ---");
+
+Reserva reserva2 = new Reserva(diasReservados: 10);
+reserva2.CadastrarSuite(suite);
+reserva2.CadastrarHospedes(hospedes);
+
+Console.WriteLine($"Hóspedes: {reserva2.ObterQuantidadeHospedes()}");
+Console.WriteLine($"Valor diária: {reserva2.CalcularValorDiaria()}");
+
+Console.WriteLine("\n--- Teste de Exception (capacidade excedida) ---");
+
+try
+{
+    Pessoa p3 = new Pessoa(nome: "Hóspede 3");
+    hospedes.Add(p3);
+    
+    Reserva reserva3 = new Reserva(diasReservados: 5);
+    reserva3.CadastrarSuite(suite);
+    reserva3.CadastrarHospedes(hospedes);
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Erro: {ex.Message}");
+}
